2c31bf1f739e9fdcb75df19f898da9a694c6105f
Bryan Bosire <bryanbosire@gmail.com>
2022-09-06 04:48:56 +0300

    commit tree 059449293803318e76b2ca235c7cc2af3f05c05f

--- countdown.rb
+++ countdown.rb
@@ -1,4 +1,22 @@
 #write your code here
 
-def countdown
+def countdown(count)
+
+    while count > 0
+        if count == 12
+         return "HAPPY NEW YEAR!" 
+        else 
+          puts  "#{count} SECOND(S)!"
+        end
+        count -= 1
+    end
 end
+
+def countdown_with_sleep(count)
+    while count > 0
+        sleep count
+        puts "#{count}"
+        count -= 1
+    end
+end
+countdown_with_sleep(5)
